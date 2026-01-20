efcd833c090673961f7e62725bcbe101a888de13
Rodolfo Castro <soyracm@gmail.com>
2026-01-20 11:19:20 -0400

    useapp...

--- projects/09-redux-toolkit/src/app/store.ts
+++ projects/09-redux-toolkit/src/app/store.ts
@@ -4,5 +4,11 @@ import tasksReducer from '../features/tasks/taskSlice'
 // El configureStore es la tienda o almacen de estado de redux, 
 // es como definir un useState aparte de la aplicacion
 export const store = configureStore({
-  tasks:tasksReducer,
+  reducer: {
+    tasks:tasksReducer,
+  }
 })
+
+// Tipos derivados del store
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
