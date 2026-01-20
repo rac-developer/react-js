3e4619b8a68fca0f8195a535adcb805478378f9e
Rodolfo Castro <soyracm@gmail.com>
2026-01-20 12:26:52 -0400

    test

--- projects/09-redux-toolkit/src/App.tsx
+++ projects/09-redux-toolkit/src/App.tsx
@@ -1,15 +1,6 @@
 import './App.css'
 
-// useApp... Es que viene tipado por defecto
-// El useDispatch va a hacer las funciones que nosotros queramos llamar para poder actualizar el estado, en pocas palabras es para hacer algo
-// El useSelector es la forma en la que vamos a poder traer los datos que esten dentro del estado, en pocas palabras es para seleccionar o traer algo
-import { useAppDispatch, useAppSelector } from './app/hooks'
-
 function App() {
-  const tasksState = useAppSelector(state => state.tasks)
-  // Es lo mismo que hacer un useState y verlo por consola, la diferencia es que si lo creo en otro componente voy a poder accerderlo
-  console.log(tasksState)
-  const dispatch = useAppDispatch()
 
   return (
     <>
--- projects/09-redux-toolkit/src/components/TaskForm.tsx
+++ projects/09-redux-toolkit/src/components/TaskForm.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const TaskForm = () => {
   return (
     <div>
